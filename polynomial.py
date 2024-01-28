4cccb0299d12beb1408146b142cc0ab5519c1355
Ken Feng <fengxk@bu.edu>
2024-01-28 16:59:16 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -7,6 +7,9 @@ class X:
 
     def __repr__(self):
         return "X"
+    
+    def evaluate(self, x):
+        return x
 
 class Int:
     def __init__(self, i):
@@ -14,6 +17,9 @@ class Int:
     
     def __repr__(self):
         return str(self.i)
+    
+    def evaluate(self, x):
+        return self.i
 
 class Add:
     def __init__(self, p1, p2):
@@ -22,6 +28,9 @@ class Add:
 
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) + self.p2.evaluate(x)
 
 class Sub:
     def __init__(self, p1, p2):
@@ -31,6 +40,9 @@ class Sub:
     def __repr__(self):
         return "( " + repr(self.p1) + " ) - ( " + repr(self.p2) + " )"
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) - self.p2.evaluate(x)
+    
 class Div:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -38,6 +50,9 @@ class Div:
     
     def __repr__(self):
         return "( " + repr(self.p1) + " ) / ( " + repr(self.p2) + " )"
+    
+    def evaluate(self, x):
+        return self.p1.evaluate(x) / self.p2.evaluate(x)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -53,8 +68,11 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
+    def evaluate(self, x):
+        return self.p1.evaluate(x) * self.p2.evaluate(x)
+    
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
 
-poly_with_div_and_sub = Add(Div(poly, Int(2)), Sub(X(), Int(1)))
-print(poly_with_div_and_sub)
+result = poly.evaluate(-1)
+print("Result is", result)
